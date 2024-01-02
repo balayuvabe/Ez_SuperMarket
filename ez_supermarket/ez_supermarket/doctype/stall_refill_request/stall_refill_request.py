412770bbe6c87adb57a913e2caafcee65653c37b
balayuvabe <balamurugan@yuvabe.com>
2024-01-02 17:18:54 +0530

    Stall Request Added

--- ez_supermarket/ez_supermarket/doctype/stall_refill_request/stall_refill_request.py
+++ ez_supermarket/ez_supermarket/doctype/stall_refill_request/stall_refill_request.py
@@ -5,97 +5,86 @@ import frappe
 import json
 from frappe.model.document import Document
 from frappe import _
-from frappe.utils import nowdate, get_time, get_datetime
+from frappe.utils import nowdate, get_time, get_datetime, today
 
 class StallRefillRequest(Document):
 	pass
 
 # @frappe.whitelist()
-# def get_sales_data(posting_date, posting_time):
-#     # Fetch sales data from Sales Invoice
-#     sales_data = frappe.get_all(
-#         "Sales Invoice",
-#         filters={
-#             "posting_date": posting_date,
-#             "posting_time": posting_time
-#         },
-#         fields=["item_code", "qty", "total_amount"]  # Specify the fields you want to fetch
-#     )
+# def fetch_items_sold(timestamp):
 
-#     # Prepare data for Stall Request Details
-#     stall_request_details = []
-#     for sale in sales_data:
-#         # Fetch current available quantity (you may need to adjust this query based on your warehouse logic)
-#         current_qty = frappe.db.get_value(
-#             "Stock Ledger Entry",
-#             {
-#                 "item_code": sale.item_code,
-#                 "warehouse": "Your Warehouse",  # Replace with your warehouse
-#                 "posting_date": posting_date,
-#                 "posting_time": posting_time
-#             },
-#             "SUM(actual_qty)"
-#         ) or 0
+#   data = []
 
-#         # Append data to stall_request_details list
-#         stall_request_details.append({
-#             "item_code": sale.item_code,
-#             "qty_sold": sale.qty,
-#             "current_qty": current_qty
-#         })
+#   invoices = frappe.db.sql("""
+#     SELECT
+#       sii.item_code, sii.qty 
+#     FROM `tabSales Invoice Item` sii
+#     INNER JOIN `tabSales Invoice` si ON sii.parent = si.name
+#     WHERE
+#       si.posting_date < %s
+#       AND si.docstatus = 1
+#   """, timestamp, as_dict=1)
 
-#     return stall_request_details
+#   items_sold = {}
 
+#   for d in invoices:
+#     item_code = d.item_code
+    
+#     if item_code not in items_sold:
+#       items_sold[item_code] = {
+#         "qty": 0
+#       }
 
-# your_module/doctype/stall_request/stall_request.py
+#     items_sold[item_code]["qty"] += d.qty
 
-# @frappe.whitelist()
-# def fetch_sales_invoices(posting_date, timestamp):
-#     # Fetch sales invoices based on posting_date and timestamp
-#     invoices = frappe.get_all(
-#         'Sales Invoice',
-#         filters={
-#             'posting_date': posting_date,
-#             'timestamp': ('<=', timestamp)
-#         },
-#         fields=['name', 'item_code', 'qty', 'total', 'location', 'warehouse']
-#     )
+#   for item_code, value in items_sold.items():
+  
+#     item = frappe.get_doc("Item", item_code)
 
-#     # Prepare a list of dictionaries with required data
-#     invoice_list = []
-#     for invoice in invoices:
-#         invoice_dict = {
-#             'item_code': invoice.item_code,
-#             'qty_sold': invoice.qty,
-#             'total': invoice.total,
-#             'location': invoice.location,
-#             'warehouse': invoice.warehouse
-#         }
-#         invoice_list.append(invoice_dict)
-
-#     return invoice_list
-
+#     data.append({
+#       "item_code": item_code,
+#       "qty_sold": value["qty"],
+#       "stall_location": item.custom_stall_location,
+#       "store_location": item.custom_store_location,
+#       "max_qty": item.custom_max_qty
+#     })
+  
+#   return data
+# Python Function
 @frappe.whitelist()
-def update_stall_request(docname):
+def fetch_items_sold(posting_date, timestamp):
+    data = []
 
-    # Load the document
-    doc = frappe.get_doc('Stall Refill Request', docname)
+    invoices = frappe.db.sql("""
+        SELECT sii.item_code, sii.qty
+        FROM `tabSales Invoice Item` sii
+        INNER JOIN `tabSales Invoice` si ON sii.parent = si.name
+        WHERE
+            si.posting_date BETWEEN %s AND %s
+            AND si.docstatus = 1
+    """, (posting_date, timestamp), as_dict=1)
 
-    # Set up posting date and time in the timestamp field
-    timestamp = frappe.utils.now_datetime()
-    doc.timestamp = timestamp
+    items_sold = {}
 
-    # Get today's date
-    date = nowdate()
+    for d in invoices:
+        item_code = d.item_code
+        
+        if item_code not in items_sold:
+            items_sold[item_code] = {
+                "qty": 0
+            }
 
-    # Fetch all the sales invoices for today until the timestamp
-    invoices = frappe.get_all('Sales Invoice', filters={'posting_date': ['>=', date], 'posting_date': ['<=', get_datetime(timestamp)]}, fields=['name', 'item_code', 'qty'])
+        items_sold[item_code]["qty"] += d.qty
 
-    # Iterate over the fetched invoices and add them to the child table
-    for invoice in invoices:
-        doc.append('stall_request_details', {
-            'item_code': invoice.item_code,
-            'qty_sold': invoice.qty
-        })
+    for item_code, value in items_sold.items():
+        item = frappe.get_doc("Item", item_code)
 
-    # Don't save the document here. You can save it manually after the updates are made.
+        data.append({
+            "item_code": item_code,
+            "qty_sold": value["qty"],
+            "stall_location": item.custom_stall_location,
+            "store_location": item.custom_store_location,
+            "max_qty": item.custom_max_qty
+        })     
+
+    return data
