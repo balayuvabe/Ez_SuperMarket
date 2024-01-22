a94f5d96aada5c49b88925f39ed864c211496c03
balayuvabe <balamurugan@yuvabe.com>
2024-01-22 13:48:32 +0530

    Query Added Article Location

--- ez_supermarket/ez_supermarket/doctype/article_location/article_location.js
+++ ez_supermarket/ez_supermarket/doctype/article_location/article_location.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2023, Balamurugan and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Article Location", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Article Location", {
+  refresh(frm) {
+    frm.set_query("warehouse", function (frm) {
+      return {
+        filters: {
+          is_group: "0",
+        },
+      };
+    });
+  },
+});
