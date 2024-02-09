51c28355e9f6fb044b6cf94cac53fb231ed5fd0a
balayuvabe <balamurugan@yuvabe.com>
2024-02-09 15:45:47 +0530

    Enabled Check Based Features

--- ez_supermarket/ez_supermarket/doctype/clients_request/clients_request.js
+++ ez_supermarket/ez_supermarket/doctype/clients_request/clients_request.js
@@ -1,11 +1,36 @@
 // Copyright (c) 2023, Balamurugan and contributors
 // For license information, please see license.txt
 
-// Handler for the "refresh" event on the "Clients Request" form.
-// Adds a custom button "Check Availability" to the form.
-// frappe.ui.form.on("Clients Request", {
-//   refresh(frm) {
-//     frm.add_custom_button(__("Check Availability"), function () {
-
-//   },
-// });
+frappe.ui.form.on("Clients Request", {
+  onload: function (frm) {
+    // Check the value of the Client Request checkbox
+    frappe.model.with_doc(
+      "Yb Supermarket Settings",
+      "Yb Supermarket Settings",
+      function () {
+        var settings_doc = frappe.get_doc(
+          "Yb Supermarket Settings",
+          "Yb Supermarket Settings"
+        );
+        if (settings_doc.clients_request == 0) {
+          // If Client Request is not checked, hide the form and throw an error
+          $.each(frm.fields_dict, function (fieldname, field) {
+            field.df.hidden = 1;
+          });
+          frm.refresh_fields();
+          // Disable the Save button
+          frm.disable_save();
+          var settings_link = frappe.utils.get_form_link(
+            "Yb Supermarket Settings",
+            settings_doc.name
+          );
+          frappe.throw(
+            "You must enable <strong>Clients Request</strong> feature in <a href='" +
+              settings_link +
+              "'><strong>Yb Supermarket Settings</a></strong> to access this page."
+          );
+        }
+      }
+    );
+  },
+});
